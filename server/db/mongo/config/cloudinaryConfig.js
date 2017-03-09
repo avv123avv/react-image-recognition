fa754a9366db8b94bc11d0451db411a5547d93f6
max <avv123avv@gmail.com>
2017-03-09 18:11:36 +0200

    fix

--- server/db/mongo/config/cloudinaryConfig.js
+++ server/db/mongo/config/cloudinaryConfig.js
@@ -1,7 +1,7 @@
 const cloudinaryConfig = {
-    cloud_name: 'dufbvdilf',
-    api_key: '228766949911837',
-    api_secret: 'wEJMpy9c_EoIFNx7MY_DezJiMkA'
+    cloud_name: '',
+    api_key: '',
+    api_secret: ''
 };
 
 export default cloudinaryConfig;
--- server/db/mongo/config/cognitiveVisionOcrConfig.js
+++ server/db/mongo/config/cognitiveVisionOcrConfig.js
@@ -1,5 +1,5 @@
 const cognitiveVisionOcrConfig = {
-    key: 'd13de8363cf041739813c2254c8709a8'
+    key: ''
 };
 
 export default cognitiveVisionOcrConfig;
