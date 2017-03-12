ccea39b87a73c92afaa17ae348b9f51618e37355
max <avv123avv@gmail.com>
2017-03-12 19:54:03 +0200

    upload add

--- server/db/mongo/config/cloudinaryConfig.js
+++ server/db/mongo/config/cloudinaryConfig.js
@@ -1,7 +1,7 @@
 const cloudinaryConfig = {
-    cloud_name: '',
-    api_key: '',
-    api_secret: ''
+    cloud_name: 'drahxeacq',
+    api_key: '261456912614411',
+    api_secret: 'D4bc42_ZEyVA72UU_fUryBAQrQw'
 };
 
 export default cloudinaryConfig;
